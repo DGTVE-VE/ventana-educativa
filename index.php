74f9c78e44a73529735c4767de3efd0d92296d94
Israel Toledo <j.israel.toledo@gmail.com>
2015-07-20 10:03:32 -0500

    Se agregaron comentarios.

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
+
+/** Inicia la sesión */
 session_start();
 
+/** 
+ * Carga el autoload este archivo permite la autocarga de clases
+ * a la hora de que se crean objetos de cualquier clase del sistema.
+ */
 require_once 'autoload.php';
 
+/**
+ * Se ejecuta el Controlador Frontal
+ * http://www.sitepoint.com/front-controller-pattern-1/
+ */
 $frontController = new SimpleFrontController();
 $frontController->run();
 
