c6cdbf14eb87274356c7d49515bff239b7c31454
Sonia Martinez <soniamartinezctr@gmail.com>
2015-07-24 18:00:56 -0500

    Se agrego responsive a serie y parte de código de calificación

---
+++ api/v1/Serie_api.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Serie_api {
+    public function calificar () {
+        var_dump($_POST);
+    }
+}
