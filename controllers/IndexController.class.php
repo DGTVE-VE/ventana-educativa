83bb5099559f6417d027ae88fdb873e0f1107b9d
unknown <JITOLEDO@CIR2160MJ01US6S.tveducativa.mx>
2015-07-29 15:38:16 -0500

    removing file from version control

--- controllers/IndexController.class.php
+++ controllers/IndexController.class.php
@@ -11,12 +11,16 @@ class IndexController extends _BaseController{
   }
 
   public function defaultAction() {
-
       if (isset($_SESSION['usuario'])){
           include 'views/vod/indexView.php'; 
       } else {
           include 'views/loginView.php';
       }
+  }
+  
+  public function closeSession() {
+    session_destroy();
+    header('Location: /');
   }
 
 }
