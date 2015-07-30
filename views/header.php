c16fd31e73e9705b19fa98f86403a24503238df8
Sonia Martinez <soniamartinezctr@gmail.com>
2015-07-30 18:06:26 -0500

    modificación del menú

--- views/header.php
+++ views/header.php
@@ -1,36 +1,20 @@
-
-<header>
+<header class="page-header fixed-top">
     <div class="row">
         <div id="wrapper">
             <!--             Sidebar -->
             <nav class="navbar navbar-inverse navbar-fixed-top" 
                  id="sidebar-wrapper" role="navigation">
-                <ul class="nav sidebar-nav">
-                    <li class="sidebar-brand">
-                    <li> <a href="#">Lo Más Visto</a> </li>
-                    <li> <a href="#">Nuevos Lanzamientos</a> </li>
-                    <li> <a href="#">Acción</a> </li>
-                    <li> <a href="#">Aventura</a> </li>
-                    <li> <a href="#">Animación</a> </li>
-                    <li> <a href="#">Comedia</a> </li>
-                    <li> <a href="#">Cultura</a> </li>
-                    <li> <a href="#">Documentales</a> </li> 
-                    <li> <a href="#">Familia</a> </li> 
-                    <li> <a href="#">Ficción</a> </li> 
-                    <li> <a href="#">Niños</a> </li>
-                    <li> <a href="#">Películas</a> </li>
-                    <li> <a href="#">Terror</a> </li>
+                <ul class="nav sidebar-nav" data-bind="foreach:categorias">                                     
+                    <li data-bind="css: $index() == 0 ? ' sidebar-brand' : ''">                    
+                    <li><a href="#" data-bind="text:categoria"></a></li>                    
                 </ul>
             </nav>
-
-            <!--             /#sidebar-wrapper 
-            
-                         Page Content -->
-
             <div class="row navbar-fixed-top navbar-inverse">                       
                 <div class="col-md-4 col-xs-2">
-                    <a href="#"><img class="hamburger" data-toggle="offcanvas" 
-                                     src="imagenes/logove.png" alt=""></a>
+                    <a href="#"><img class="hamburger" 
+                                     data-toggle="offcanvas" 
+                                     src="imagenes/logove.png" 
+                                     alt=""></a>
                 </div>
                 <div class="col-md-2 col-xs-5 text-right">
                     <a href="#"><img src="imagenes/logoDGTVEsolo.png" alt=""></a>
@@ -38,14 +22,17 @@
                 <div class="col-md-2 visible-md visible-lg text-left" id="texto">
                     Televisión <br> Educativa
                 </div>
-                <div class="col-md-2"></div>
+                <div class="col-md-2"> 
+                    <a href="session/close" > Cerrar sesión </a>
+                </div>
                 <div class="col-md-2 pull-right col-xs-4">
                     <form role="search">
-                        <div class="input-group" id="buscar">
-                            <input type="text"  class="form-control"  placeholder="Buscar">
+                        <div class="input-group">
+                            <input type="text" class="form-control"  placeholder="Buscar">
                             <div class="input-group-btn">
                                 <button class="btn btn-default" type="submit">
-                                    <i class="glyphicon glyphicon-search"></i></button>
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -56,5 +43,6 @@
         <!--/#page-content-wrapper--> 
 
     </div>
-
-</header>  
+    <script src='http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.1.0.js'></script>
+    <script src='js/knockout.mapping-latest.js'></script>
+</header> 
