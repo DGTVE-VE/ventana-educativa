693013596058638677be889ea39c1047fd875119
Israel Toledo <j.israel.toledo@gmail.com>
2015-08-19 11:14:19 -0500

    Se agregaron imágenes y se cambió el idioma del template principal a español de méxico

--- tpl/index.tpl.php
+++ tpl/index.tpl.php
@@ -4,7 +4,7 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="es-MX">
     <head>
         <title></title>
         <base href="http://<?php print $_SESSION[CONFIG]['host']['url']; ?>">
