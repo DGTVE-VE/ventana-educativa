e76105f479691d6f77de948af8fc9372924ab70f
unknown <JITOLEDO@CIR2160MJ01US6S.tveducativa.mx>
2015-09-23 12:51:04 -0500

    cambio de metodo logout

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -61,13 +61,23 @@
 //                });
 //            }
 
-              function signOut(){
-                  //alert('entro al metodo signOut de Google');
-                  var objetoAuth = gapi.auth2.getAuthInstance();
-                  alert(objetoAuth);
-                  var isConnected = GoogleAuth.isSignedIn.get();
-                  alert('el usuario esta conectado? ' + isConnected);
-              }
+//              function signOut(){
+//                  //alert('entro al metodo signOut de Google');
+//                  var objetoAuth = gapi.auth2.getAuthInstance();
+//                  alert(objetoAuth);
+//                  var isConnected = GoogleAuth.isSignedIn.get();
+//                  alert('el usuario esta conectado? ' + isConnected);
+//              }
+              
+              function signOut() {
+                 var auth2 = gapi.auth2.getAuthInstance();
+                 auth2.signOut().then(function () {
+                 console.log('User signed out.');
+                });
+               }
+              
+              
+              
 
             $(document).on('click', '#logout', function(){
 //                alert('click logout google');
