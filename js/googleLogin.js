3d38f0a0630d22f9c8102f3538df8c76a9edd6f2
unknown <JITOLEDO@CIR2160MJ01US6S.tveducativa.mx>
2015-09-25 12:03:33 -0500

    agregando GoogleAuth.signout();

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -60,13 +60,15 @@
                     var auth2 = gapi.auth2.getAuthInstance();
                     auth2.signOut().then(function () {
                         console.log('User signed out.');
+                        GoogleAuth.signOut();
                     });
                 }
 
                 function onLoad() {
                     gapi.load('auth2', function () {
                         gapi.auth2.init({
-                            client_id: 'CLIENT_ID.apps.googleusercontent.com'
+                         client_id: 'CLIENT_ID.apps.googleusercontent.com'
+                           
                         });
                     });
                 }
