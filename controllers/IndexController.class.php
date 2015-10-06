5be15c4d893efd45284591deb77bba0064f2b5eb
Israel Toledo <j.israel.toledo@gmail.com>
2015-10-06 16:58:13 -0500

    Se eliminaron comentarios

--- controllers/IndexController.class.php
+++ controllers/IndexController.class.php
@@ -3,11 +3,6 @@
 
 class IndexController extends _BaseController{
   
-  //public function login (){
-    //$_SESSION['loginUrl'] = UserService::createLoginUrl('/');
-    //include 'views/loginView.php';
-  //}
-
   public function defaultAction() {
       if (isset($_SESSION['usuario'])){          
           header('Location: /ventana-educativa/vod');
